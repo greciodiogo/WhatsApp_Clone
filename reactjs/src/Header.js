89a14411fca6167960f012f7cec0f9cfa77a15f0
Grecio Diogo <fonebahia8@gmail.com>
2022-03-03 21:26:12 +0100

    Criaça

---
+++ reactjs/src/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Header.css'
+import Logo from './assets/_1511452487_364_Airbnb-mobile.jpg'
+import SearchIcon from '@material-ui/icons/Search'
+import LanguageIcon from '@material-ui/icons/Language'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import {Avatar} from '@material-ui/core'
+import {Link} from 'react-router-dom'
+    function Header(){
+        return (
+            <div className='header'>
+       
+            </div>
+
+        )
+    }
+
+    export default Header
